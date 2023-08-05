1fc55af29f1ba6074afa38a8783a79d80566b0c4
Charles Taylor <charlestaylor95@gmail.com>
2023-08-05 16:35:32 -0400

    Format

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 export default function App() {
   const [count, setCount] = useState(0);
-  console.log("api key", OPEN_WEATHER_API_KEY)
+  console.log('api key', OPEN_WEATHER_API_KEY);
   return (
     <div className="flex flex-col justify-content align-items gap-5">
       <h1>Vite + React + Typescript</h1>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,2 @@
 /// <reference types="vite/client" />
-declare const OPEN_WEATHER_API_KEY: string
+declare const OPEN_WEATHER_API_KEY: string;
