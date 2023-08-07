debf2438fec8edec4591752c2f06e2c760a88af0
Charles Taylor <charlestaylor95@gmail.com>
2023-08-07 18:15:34 -0400

    Initial stab at domain model

--- src/weather-dashboard.ts
+++ src/weather-dashboard.ts
@@ -3,14 +3,31 @@ export default class WeatherDashboard {
   readonly view: View = 'chart';
   readonly forecastDays: number = 3;
 
-  constructor(fields?: Partial<Fields<WeatherDashboard>>) {
+  constructor(fields?: Fields<WeatherDashboard>) {
     if (fields !== undefined) {
       Object.assign(this, fields)
     }
   }
 
-  foo(): WeatherDashboard {
-    return new WeatherDashboard();
+  changeView(view: View): WeatherDashboard {
+    return new WeatherDashboard({
+      ...this,
+      view,
+    });
+  }
+
+  changeForecastDays(forecastDays: number): WeatherDashboard {
+    return new WeatherDashboard({
+      ...this,
+      forecastDays,
+    })
+  }
+
+  appendCity(city: City): WeatherDashboard {
+    return new WeatherDashboard({
+      ...this,
+      cities: [...this.cities, city]
+    })
   }
 }
 
@@ -25,9 +42,3 @@ export type City = {
 
 type FieldNames<T> = { [K in keyof T]: T[K] extends Function ? never : K }[keyof T];
 type Fields<T> = { [K in FieldNames<T>]: T[K] };
-
-const foo: Fields<WeatherDashboard> = {
-  view: 'chart',
-  cities: [],
-  forecastDays: 3,
-};
