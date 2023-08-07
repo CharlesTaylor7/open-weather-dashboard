9ccd8975162c44299bb2531bc5c031d5d47f780a
Charles Taylor <charlestaylor95@gmail.com>
2023-08-07 13:16:30 -0400

    center dashboard on page

--- src/components/Mockup.tsx
+++ src/components/Mockup.tsx
@@ -2,25 +2,29 @@ import TimeSeriesLineChart from '@/components/TimeSeriesLineChart';
 
 export default function Mockup() {
   return (
-    <div className="flex flex-col gap-5 m-5 max-w-lg justify-center items-start">
-      <header className="self-center bold text-2xl">Open Weather Dashboard</header>
-      <div className="flex gap-2 items-center w-full">
-        <label className="block grow-0">City, State</label>
-        <input className="block grow border rounded" />
-        <button className="block grow-0 flex items-center justify-center px-2 border rounded-2xl">
-          Search
-        </button>
-      </div>
-      <div className="flex flex-wrap gap-2">
-        <Pill label="Chattanooga" />
-        <Pill label="Knoxville" />
-        <Pill label="Cleveland" />
-        <Pill label="Atlanta" />
-      </div>
-      <TimeSeriesLineChart data={[]} />
-      <div className="flex gap-3">
-        <ButtonToggle label="Table View" />
-        <ButtonToggle label="3 Day View" />
+    <div className="flex w-full justify-center items-center">
+      <div className="flex flex-col gap-5 m-5 max-w-lg justify-center items-start">
+        <header className="self-center bold text-2xl">
+          Open Weather Dashboard
+        </header>
+        <div className="flex gap-2 items-center w-full">
+          <label className="block grow-0">City, State</label>
+          <input className="block grow border rounded" />
+          <button className="block grow-0 flex items-center justify-center px-2 border rounded-2xl">
+            Search
+          </button>
+        </div>
+        <div className="flex flex-wrap gap-2">
+          <Pill label="Chattanooga" />
+          <Pill label="Knoxville" />
+          <Pill label="Cleveland" />
+          <Pill label="Atlanta" />
+        </div>
+        <TimeSeriesLineChart data={[]} />
+        <div className="flex gap-3">
+          <ButtonToggle label="Table View" />
+          <ButtonToggle label="3 Day View" />
+        </div>
       </div>
     </div>
   );
