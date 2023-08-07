fb65bd11f3e823b17f75d06364bfa53226a2cfc9
Charles Taylor <charlestaylor95@gmail.com>
2023-08-07 11:43:03 -0400

    Basic pill design

--- src/components/Mockup.tsx
+++ src/components/Mockup.tsx
@@ -3,26 +3,34 @@ import TimeSeriesLineChart from '@/components/TimeSeriesLineChart';
 export default function Mockup() {
   return (
     <div className="flex flex-col gap-5 mt-5 w-full justify-center items-center">
-      <header>Open Weather Dashboard</header>
+      <header className="bold text-2xl"> Open Weather Dashboard</header>
       <div>
-        <label>City, State</label>
-        <input />
+        <label className="mr-2">City, State</label>
+        <input className="border" />
       </div>
-      <div>
-        <div className="inline-block">
-          <span>Chattanooga</span>
-          <button>x</button>
-        </div>
-        <div className="inline-block">
-          <span>Knoxville</span>
-          <button>x</button>
-        </div>
+      <div className="flex flex-wrap gap-2">
+        <Pill label="Chattanooga" />
+        <Pill label="Knoxville" />
+        <Pill label="Cleveland" />
+        <Pill label="Atlanta" />
       </div>
       <TimeSeriesLineChart data={[]} />
       <div>
         <button>Table View</button>
         <button>3 Day View</button>
       </div>
+    </div>
+  );
+}
+
+type PillProps = {
+  label: string;
+};
+function Pill(props: PillProps) {
+  return (
+    <div className="flex items-center border rounded-3xl p-1 gap-2">
+      <span>{props.label}</span>
+      <button className="flex items-center justify-center h-5 w-5 border rounded-full">×</button>
     </div>
   );
 }
