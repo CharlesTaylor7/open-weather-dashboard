3622403ac5ff24488072109d17a605458c4b83fb
Charles Taylor <charlestaylor95@gmail.com>
2023-08-07 13:18:48 -0400

    Restyle pills

--- src/components/Mockup.tsx
+++ src/components/Mockup.tsx
@@ -47,9 +47,9 @@ type PillProps = {
 };
 function Pill(props: PillProps) {
   return (
-    <div className="flex items-center border rounded-3xl p-1 gap-2">
+    <div className="flex items-center border rounded-3xl px-2 py-1 gap-2 bg-slate-200">
       <span>{props.label}</span>
-      <button className="flex items-center justify-center h-5 w-5 border rounded-full">
+      <button className="flex items-center justify-center h-5 w-5 border rounded-full hover:bg-red-200">
         ×
       </button>
     </div>
