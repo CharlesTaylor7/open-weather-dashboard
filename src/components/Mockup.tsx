fddd0827c8519c724c920bc410dfe5949ecdb36e
Charles Taylor <charlestaylor95@gmail.com>
2023-08-07 11:21:42 -0400

    First stab at mockup

---
+++ src/components/Mockup.tsx
@@ -0,0 +1,28 @@
+import TimeSeriesLineChart from '@/components/TimeSeriesLineChart';
+
+export default function Mockup() {
+  return (
+    <div className="flex flex-col gap-5 mt-5 justify-content align-items">
+      <header>Open Weather Dashboard</header>
+      <div>
+        <label>City, State</label>
+        <input />
+      </div>
+      <div>
+        <div className="inline-block">
+          <span>Chattanooga</span>
+          <button>x</button>
+        </div>
+        <div className="inline-block">
+          <span>Knoxville</span>
+          <button>x</button>
+        </div>
+      </div>
+      <TimeSeriesLineChart data={[]} />
+      <div>
+        <button>Table View</button>
+        <button>3 Day View</button>
+      </div>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/components/App.tsx';
+import Mockup from '@/components/Mockup.tsx';
 import '../styles/tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Mockup />
   </React.StrictMode>,
 );
