667387b3f49aa07a396d1d663fda197732f2b2af
Charles Taylor <charlestaylor95@gmail.com>
2023-08-07 17:55:45 -0400

    Weather dashboard domain model

--- src/weather-dashboard.ts
+++ src/weather-dashboard.ts
@@ -1,1 +1,33 @@
-export default class WeatherDashboard {}
+export default class WeatherDashboard {
+  readonly cities: Array<City> = [];
+  readonly view: View = 'chart';
+  readonly forecastDays: number = 3;
+
+  constructor(fields?: Partial<Fields<WeatherDashboard>>) {
+    if (fields !== undefined) {
+      Object.assign(this, fields)
+    }
+  }
+
+  foo(): WeatherDashboard {
+    return new WeatherDashboard();
+  }
+}
+
+export type View = 'chart' | 'table';
+export type City = {
+  label: string;
+  data: Array<{
+    date: Date;
+    temperature: number;
+  }>;
+};
+
+type FieldNames<T> = { [K in keyof T]: T[K] extends Function ? never : K }[keyof T];
+type Fields<T> = { [K in FieldNames<T>]: T[K] };
+
+const foo: Fields<WeatherDashboard> = {
+  view: 'chart',
+  cities: [],
+  forecastDays: 3,
+};
