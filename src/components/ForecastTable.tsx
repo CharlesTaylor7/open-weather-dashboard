b45a34276254662cf9a274b3114f1bb1a10e4950
Charles Taylor <charlestaylor95@gmail.com>
2023-08-08 19:56:02 -0400

    Formatted

--- src/components/ForecastTable.tsx
+++ src/components/ForecastTable.tsx
@@ -14,7 +14,9 @@ export default function ForecastTable(props: Props) {
       rowCount={props.data.length}
       columnCount={props.data[0]?.data?.length || 0}
       formatRowHeader={(rowIndex) => props.data[rowIndex].name}
-      formatColumnHeader={(columnIndex) => formatDate(props.data[0].data[columnIndex].x)}
+      formatColumnHeader={(columnIndex) =>
+        formatDate(props.data[0].data[columnIndex].x)
+      }
       formatCell={({ rowIndex, columnIndex }) =>
         `${props.data[rowIndex].data[columnIndex].y.toFixed(0)}℉`
       }
